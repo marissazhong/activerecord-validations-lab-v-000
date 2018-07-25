d46cc19df13faa22a3886ddb58efe2b6eb63910b
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 15:10:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :title, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"]}
+
 end
