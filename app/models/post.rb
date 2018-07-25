cef2c630912ec5e4846b42e0c7dd061141f8a884
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 15:34:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait
+
+  @@clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]/i, /Guess/i]
+
+  def is_clickbait
+    if !@@clickbait.any? {|word| word.match title}
+      errors.add(:title, "not clickbait")
+    end
+  end
+
 end
