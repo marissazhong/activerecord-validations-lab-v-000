e541d29d36e1fdad5b4b8ca0320da888e2cce971
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 15:25:20 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validates :title, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"]}
+  validate :is_clickbait
 
   clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
 
   def is_clickbait
-
+    if !clickbait.any {|word| title.include?(word)}
+      
   end
 
 end
