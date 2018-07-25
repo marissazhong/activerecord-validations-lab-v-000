543eecc8057b954f2172f1f2286d8e78cfd78480
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 15:08:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-
+  validates :category, 
 end
